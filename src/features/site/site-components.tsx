1672ea314cc9570898efca8cc43ccacb6d84adc3
raihahahan <mraihandev@gmail.com>
2023-08-28 13:00:50 +0800

    testing changes

--- src/features/site/site-components.tsx
+++ src/features/site/site-components.tsx
@@ -135,6 +135,7 @@ export function CustomAnchor({
 export function MyFooter({ extraStyles }: { extraStyles?: CSSProperties }) {
   const { siteColors: colors } = useTheme();
   const { sm } = useGlobalMediaQuery();
+
   return (
     <section
       id="FOOTER"
